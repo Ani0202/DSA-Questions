494f260b0e0ed42462abdc36772ff0f889c53189
Aniket Agrawal <agrawalaniket02@gmail.com>
2021-09-24 00:17:38 +0530

    Update Prob2.py

--- Arrays/Prob2.py
+++ Arrays/Prob2.py
@@ -9,9 +9,7 @@
     (x+1, y-1), 
     (x+1, y)  , 
     (x+1, y+1) 
-You are given a sequence of points and the order in which you need to cover the points.. Give the minimum number of steps in which you can achieve it. You start from the first point.
-
-NOTE: This question is intentionally left slightly vague. Clarify the question by trying out a few cases in the “See Expected Output” section.'''
+You are given a sequence of points and the order in which you need to cover the points.. Give the minimum number of steps in which you can achieve it. You start from the first point.'''
 
 class Solution:
     # @param A : list of integers
