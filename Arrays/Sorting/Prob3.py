c87c8d4c65af89c03415a030c33e7301dcae8030
Ani0202 <agrawalaniket02@gmail.com>
2021-10-12 23:38:49 +0530

    Array Questions

--- Arrays/Sorting/Prob3.py
+++ Arrays/Sorting/Prob3.py
@@ -1,4 +1,5 @@
-'''A hotel manager has to process N advance bookings of rooms for the next season. His hotel has C rooms. Bookings contain an arrival date and a departure date. He wants to find out whether there are enough rooms in the hotel to satisfy the demand. Write a program that solves this problem in time O(N log N) .'''
+'''A hotel manager has to process N advance bookings of rooms for the next season. His hotel has C rooms. Bookings contain an arrival date and a departure date. 
+He wants to find out whether there are enough rooms in the hotel to satisfy the demand. Write a program that solves this problem in time O(N log N) .'''
 
 class Solution:
     # @param arrive : list of integers
